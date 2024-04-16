9ec52938c0af28e34e80a797daedc199d9c8cc16
kiv1n <aloha@kiv1n.ru>
2024-04-16 18:34:23 +0300

    Deploy contracts

--- contracts/scripts/data/deployed-contracts.ts
+++ contracts/scripts/data/deployed-contracts.ts
@@ -8,8 +8,8 @@ export const CONTRACTS: {
   };
 } = {
   morphTestnet: {
-    aiApp: undefined,
-    usdToken: undefined,
+    aiApp: "0x96E6AF6E9e400d0Cd6a4045F122df22BCaAAca59",
+    usdToken: "0x02008a8DBc938bd7930bf370617065B6B0c1221a",
     entryPoint: undefined,
     accountFactory: undefined,
     paymaster: undefined,
